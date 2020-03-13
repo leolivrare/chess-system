79c98d03d2c68b8bb91fe902a000ab5e0514b8c1
leolivrare <l220120@dac.unicamp.br>
2020-03-13 16:22:34 -0300

    Chess layer e UI class

---
+++ src/chesslayer/ChessPiece.java
@@ -0,0 +1,26 @@
+package chesslayer;
+
+import boardlayer.Board;
+import boardlayer.Piece;
+
+public class ChessPiece extends Piece{
+	
+	private Color color;
+	private int moveCount;
+	
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+
+	public Color getColor() {
+		return color;
+	}
+
+
+	
+	
+	
+}
